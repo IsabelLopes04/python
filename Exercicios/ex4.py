612bcb544101d8271831c4478aca1f76f5b7f0e8
ISABEL DOS SANTOS LOPES <isabel04lopes@gmail.com>
2026-09-03 22:08:18 -0300

    Refactor variable names for clarity in prime number counting script

--- Exercicios/ex4.py
+++ Exercicios/ex4.py
@@ -1,16 +1,16 @@
 # Conte quantos números primos menores que 1.000.000.000 existem.
 
-def primo(numero):
-    if numero < 2:
+def primo(n):
+    if n < 2:
         return False
-    for i in range(2, numero):
-        if numero % i == 0:
+    for i in range(2, n):
+        if n % i == 0:
             return False
     return True
 
 contador = 0
-for n in range (1,1000000000):
-    if primo(n):
+for x in range (1,1000000000):
+    if primo(x):
         contador += 1
 
 print(f" A quantidadde de números é: " ,contador)
